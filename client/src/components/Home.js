cfa2bc7da222b4554913329978d8e0131ceab599
Stanley SkimZ Kimambo <Stanleykimambo@gmail.com>
2024-02-04 23:55:12 +0300

    Completed assignment

--- client/src/components/Home.js
+++ client/src/components/Home.js
@@ -8,7 +8,7 @@ function Home() {
   useEffect(() => {
     fetch("/articles")
       .then((r) => r.json())
-      .then(setArticles);
+      .then((d) => setArticles(d));
   }, []);
 
   return (
--- server/app.py
+++ server/app.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python3
 
-from flask import Flask, make_response, jsonify, session
+from flask import Flask, make_response, jsonify, session, request, json
+from flask_cors import CORS
 from flask_migrate import Migrate
 
 from models import db, Article, User
@@ -10,25 +11,51 @@ app.secret_key = b'Y\xf1Xz\x00\xad|eQ\x80t \xca\x1a\x10K'
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///app.db'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 app.json.compact = False
+CORS(app)
 
 migrate = Migrate(app, db)
 
 db.init_app(app)
 
-@app.route('/clear')
-def clear_session():
-    session['page_views'] = 0
-    return {'message': '200: Successfully cleared session data.'}, 200
+# @app.route('/clear')
+# def clear_session():
+#     session['page_views'] = 0
+#     return {'message': '200: Successfully cleared session data.'}, 200
 
 @app.route('/articles')
 def index_articles():
+    article = Article.query.filter_by(id=id).first()
+    session['page_views'] = session.get('page_views', 0) + 1
+    
+    if article:
+        article_client = {
+            'title': article.title,
+            'author': article.author,
+            'date': article.date,
+            'content': article.content,
+            'minutes_to_read': article.minutes_to_read
+        }
 
-    pass
+        
+        response_data = {
+            'session': {
+                'session_key': 'page_views',
+                'session_value': session['page_views'],
+                'session_accessed': session.accessed,
+            },
+            'cookies': [{cookie: request.cookies[cookie]} for cookie in request.cookies],
+            'article': article_client,
+        }
 
-@app.route('/articles/<int:id>')
-def show_article(id):
+        response = make_response(jsonify(response_data), 200)
+        session.get('page_views')
 
-    pass
+        # Set the 'article' cookie with the article data
+        response.set_cookie('article', value=json.dumps(article_client))  # Adjust max_age as needed
+
+        return response
+    return response
+
 
 if __name__ == '__main__':
     app.run(port=5555)
